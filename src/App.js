2c22f2a9c36d97dce448f93bf250d75794e3ea31
harshithareddy123456 <vsharshi233@gmail.com>
2024-05-10 22:00:32 +0530

    counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState(0);
+  const [input, setInput] = useState(0);
+  const handleadd = () => {
+    if (input === 0) {
+      let add = value + 1;
+      setValue(add);
+    } else {
+      let add = value + input;
+      setValue(add);
+    }
+  };
+  const handlesub = () => {
+    if (input === 0) {
+      let sub = value - 1;
+      setValue(sub);
+    } else {
+      let sub = value - input;
+      setValue(sub);
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter</h1>
+      <div style={{ fontSize: "30px", fontWeight: "bold" }}>{value}</div>
+      <div className="main-container">
+        <button className="button" onClick={handleadd}>
+          +
+        </button>
+        <button className="button" onClick={handlesub}>
+          -
+        </button>
+        <input
+          type="number"
+          value={input}
+          style={{ height: "30px" }}
+          onChange={(e) => setInput(parseInt(e.target.value))}
+        ></input>
+      </div>
     </div>
   );
 }
